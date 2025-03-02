93b6314898e30495c5ef54c7afe8d505ade964fa
ranab123 <theranabanankhah@gmail.com>
2025-03-02 11:54:40 -0800

    font fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react"
-import { MenuSelector } from "./MenuSelector"
+import { MenuSelector } from "./menuSelector"
 import { FeedbackForm } from "./FeedbackForm"
-import { MealUploader } from "./MealUploader";
-import './App.css' // Import the CSS file
+import { MealUploader } from "./MealUploader"
+import './App.css'
+import barryLeft from './assets/Barry-B-Benson.webp'
+import barryRight from './assets/Barry_B._Benson.png'
 
 function App() {
+  // Set the CSS variables with the imported image URLs
+  document.documentElement.style.setProperty('--barry-left', `url(${barryLeft})`);
+  document.documentElement.style.setProperty('--barry-right', `url(${barryRight})`);
+
   return (
     <div className="app-container">
       <header>
